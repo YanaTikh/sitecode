7ce99f084eeed1b216ec281e05a6f6c9a591aba3
YanaTikh <132406849+YanaTikh@users.noreply.github.com>
2023-05-15 02:53:53 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -19,7 +19,23 @@ if (minutes < 10) {
 }
 let today = document.querySelector("#date");
 today.innerHTML = day + " " + hours + ":" + minutes;
-
+function showInfo(response) {
+  celsiusTemp = response.data.main.temp;
+  let temp = Math.round(response.data.main.temp);
+  let grad = document.querySelector("#grad");
+  grad.innerHTML = temp;
+  let description = document.querySelector("#description");
+  description.innerHTML = response.data.weather[0].description;
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = response.data.main.humidity;
+  let windSpeed = document.querySelector("#wind");
+  windSpeed.innerHTML = Math.round(response.data.wind.speed);
+  let icon = document.querySelector("#icon");
+  icon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+}
 function search(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#input-city");
@@ -34,16 +50,13 @@ function search(event) {
   let apiKey = "220f7884705091279476bbcd1679b0dc";
   let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
   apiUrl = apiUrl + searchInput.value + "&appid=" + apiKey + "&units=metric";
-  function showInfo(response) {
-    let temp = Math.round(response.data.main.temp);
-    let grad = document.querySelector("#grad");
-    grad.innerHTML = temp;
-  }
+
   axios.get(apiUrl).then(showInfo);
 }
+
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
-
+let celsiusTemp = null;
 function showPosition(position) {
   let longitude = position.coords.longitude;
   let latitude = position.coords.latitude;
@@ -64,3 +77,22 @@ function getCurrentPosition() {
 }
 let button = document.querySelector("#currentLocation");
 button.addEventListener("click", getCurrentPosition);
+function displayFahrenheitTemp(event) {
+  event.preventDefault();
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let tempElement = document.querySelector("#grad");
+  let fahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
+  tempElement.innerHTML = Math.round(fahrenheitTemp);
+}
+function displayCelsiusTemp(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let tempElement = document.querySelector("#grad");
+  tempElement.innerHTML = Math.round(celsiusTemp);
+}
+let fahrenheitLink = document.querySelector(".fahrenheitLink");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemp);
+let celsiusLink = document.querySelector(".celsiusLink");
+celsiusLink.addEventListener("click", displayCelsiusTemp);
